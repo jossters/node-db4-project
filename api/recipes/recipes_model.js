61c15cda94e1f5f428c9b9614178a692e23ec9ac
Johnny Murillo <jossters@gmail.com>
2021-06-10 22:36:17 -0700

    wip

--- api/recipes/recipes_model.js
+++ api/recipes/recipes_model.js
@@ -1,4 +1,8 @@
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`ayy${recipe_id}`)
+const db = require('../../data/db-config')
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+    .where('recipe_id', recipe_id)
+    return recipeRows
 }
 module.exports = { getRecipeById}
--- data/db-config.js
+++ data/db-config.js
@@ -1,4 +1,4 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 const environment = process.env.NODE_ENV
-module.exports = knex(config.[environment]);
+module.exports = knex(config[environment]);
